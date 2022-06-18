bba90edb534481ae775f4c82d33e1c12f809de87
nguyentanthai <nguyentanthai2017@gmail.com>
2022-06-18 23:41:27 +0700

    Add project files.

---
+++ DOAN_WEB_1/Controllers/IphoneStoreController.cs
@@ -0,0 +1,26 @@
+﻿using DOAN_WEB_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOAN_WEB_1.Controllers
+{
+    public class IphoneStoreController : Controller
+    {
+        // GET: IphoneStore
+        dbIphoneDataContext data = new dbIphoneDataContext();
+
+        public ActionResult Index()
+        {
+            var iphonemoi = LayIphoneMoi(7);
+            return View(iphonemoi);
+        }
+        private List<SanPham> LayIphoneMoi(int count)
+        {
+            return data.SanPhams.OrderByDescending(a => a.MaSP).Take(count).ToList();
+         }
+
+    }
+}
---
+++ DOAN_WEB_1/Controllers/NguoiDungController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOAN_WEB_1.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+    }
+}
