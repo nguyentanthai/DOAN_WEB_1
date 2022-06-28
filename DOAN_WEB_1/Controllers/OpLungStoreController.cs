50c7845d9df0907e10aace02fe4fbe9d58c058a5
nguyentanthai <nguyentanthai2017@gmail.com>
2022-06-28 21:20:31 +0700

    Thêm code chi tiết sản phẩm

---
+++ DOAN_WEB_1/Controllers/OpLungStoreController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOAN_WEB_1.Controllers
+{
+    public class OpLungStoreController : Controller
+    {
+        // GET: OpLungStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
